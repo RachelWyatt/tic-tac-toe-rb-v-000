53cdbf2643dc75c2acf9e9be8eca3778beea2ed7
Rachel Wyatt <rw126@evansville.edu>
2018-08-05 23:56:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -121,7 +121,7 @@ def play(board)
   end
 
   if won?(board)
-    puts "Congratulations #{won(board)[0]}!"
+    puts "Congratulations #{won?(board)[0]}!"
   else draw?(board)
     puts "Cat's Game!"
   end
